87e7470a1a97acbff79f9ed686baf4894cd4a9fb
Knut Valen <knut.valen@icloud.com>
2020-12-21 17:08:05 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/android/clippingexample/MainActivity.kt
+++ app/src/main/java/com/example/android/clippingexample/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.android.clippingexample
 
-import android.content.Context
-import android.graphics.Paint
-import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
