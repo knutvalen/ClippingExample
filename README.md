3bcc29e78facfc2007985141fc747a7203e437c6
Knut Valen <knut.valen@icloud.com>
2020-12-21 10:16:48 +0100

    add ClippedView

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ClippingExample
+[![Build Status](https://app.bitrise.io/app/927a7f7591342907/status.svg?token=1yWhWqZKy3jO_1IUmnF0aQ)](https://app.bitrise.io/app/927a7f7591342907)
 
 This is the app for learning how to clip canvas objects by the [Android Kotlin Developer Nanodegree at Udacity](https://www.udacity.com/course/android-kotlin-developer-nanodegree--nd940).
--- app/src/main/java/com/example/android/clippingexample/MainActivity.kt
+++ app/src/main/java/com/example/android/clippingexample/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.android.clippingexample
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(ClippedView(this))
     }
+}
+
+class ClippedView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    
 }
